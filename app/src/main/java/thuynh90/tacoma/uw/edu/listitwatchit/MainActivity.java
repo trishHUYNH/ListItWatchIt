a359c4d2c5bf037f483f219c0cda3ae078aad2ac
Trish Huynh <HUYNHtrish@gmail.com>
2016-04-27 22:29:07 -0700

    Added listener to SearchView & allowed Internet permissions

--- app/src/main/java/thuynh90/tacoma/uw/edu/listitwatchit/MainActivity.java
+++ app/src/main/java/thuynh90/tacoma/uw/edu/listitwatchit/MainActivity.java
@@ -2,9 +2,11 @@ package thuynh90.tacoma.uw.edu.listitwatchit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,20 +23,52 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search Movies...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            // TODO: Submitted search
+            public boolean onQueryTextSubmit(String query) {
+                //Test search value.ToBeDeleted
+                Toast.makeText(MainActivity.this, "Search value: " + query, Toast.LENGTH_SHORT).show();
+                if( ! searchView.isIconified()) {
+                    searchView.setIconified(true);
+                }
+                return false;
+            }
+            @Override
+            // TODO: Suggested results
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
         return true;
     }
 
     @Override
+
+
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        // TODO: Make decisions on Settings
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
+        // TODO: Implement Logout
+        if (id == R.id.logout) {
+
+        }
+
+
 
         return super.onOptionsItemSelected(item);
     }
